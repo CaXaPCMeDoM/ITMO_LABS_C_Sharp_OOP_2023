ed6954db90a9c1b01844cccfa5debed57694444d
CaXaPCMeDoM <114598862+CaXaPCMeDoM@users.noreply.github.com>
2023-11-18 11:59:03 +0300

    test: added a class that allows the program to run while commands are being served

---
+++ src/Lab4/StartCommand/Start.cs
@@ -0,0 +1,22 @@
+using System;
+using Itmo.ObjectOrientedProgramming.Lab4.CommandChainOfResponsibility;
+using Itmo.ObjectOrientedProgramming.Lab4.CommandChainOfResponsibility.Base;
+
+namespace Itmo.ObjectOrientedProgramming.Lab4.StartCommand;
+
+public static class Start
+{
+    public static void Programm()
+    {
+        while (true)
+        {
+            string? message = Console.ReadLine();
+            var chairOfCommand = new ChairOfCommand();
+            if (message != null)
+            {
+                Request request = Parse.Parser.ParserRequest(message);
+                chairOfCommand.AssemblingTheChain(request);
+            }
+        }
+    }
+}
